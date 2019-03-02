f7e160d0223525c80ad746c2b37838a6c4174748
chrisbostrom <chris@porcas.com>
2019-03-01 23:19:43 -0800

    add working Utils.java

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,10 @@
  * @author chrisbostrom
  */
 public class Main {
+    public static void main(String[] args) {
+
+        String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        System.out.println(data);
+    }
 }
+
